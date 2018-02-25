09135124a82bb4c11fad966e56c5e4724c5f55f7
robert allaway <robert.j.allaway@gmail.com>
2018-02-24 22:37:46 -0800

    disclaimer

--- ui.R
+++ ui.R
@@ -14,14 +14,23 @@ ui <- fluidPage(
                 language = "en", width = NULL),
       dateInput("enddate", "End Date", value = NULL, min = NULL, max = NULL,
                 format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-                language = "en", width = NULL)
-    ),
+                language = "en", width = NULL),
+      br(),
+      br(), 
+      br(), 
+      fluidRow(h5("Please note, all the information provided on this website is provided 'as-is' and with no warranties or guarantees of accuracy.")),
+      br(),  
+      br(),  
+      br(),
+      a("Feature Suggestion Box", href = "http://freesuggestionbox.com/pub/wefwwwr")
+   ),
     
     mainPanel(
       fluidRow(h4(textOutput("length"))),
       fluidRow(h4(textOutput("overlap"))),
       fluidRow(h4(textOutput("percentoccupancy")))
-             
+      
+
     )
   )
 )
